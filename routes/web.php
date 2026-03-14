1629a2916a28e085021b00c5ebfb1d6557dc3266
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2026-03-14 13:09:10 +0100

    dodanie nazw do routingu

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('start');
 
 /* Route::get('/user/{id}', function (string $id) {
     return 'User '.$id;
 })->name('test'); */
 Route::get('/kontakt', function () {
     return view('kontakt');
-});
-Route::get('/onas', function () {
+})->name('kontakt');
+Route::get('/o-nas', function () {
     $zadania = [
         'Zadanie 1',
         'Zadanie 2',
         'Zadanie 3',
     ];
     return view('onas',['zadania'=>$zadania]);
-});
+})->name('onas');
