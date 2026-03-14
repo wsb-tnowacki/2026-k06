f27501d6d51ebc4e573e0f539a130936025749fa
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2026-03-14 13:53:27 +0100

    Auth Breez + naprawa menu i usunięcie dashboardu i panel usera

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,3 +21,16 @@ Route::get('/o-nas', function () {
     ];
     return view('onas',['zadania'=>$zadania]);
 })->name('onas');
+
+Route::get('/dashboard', function () {
+    //return view('dashboard');
+    return redirect(route('start'));
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
