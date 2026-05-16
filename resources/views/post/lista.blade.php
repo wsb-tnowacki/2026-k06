0a4809f284639df4a8b63eefd7823ebdd52931f9
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2026-05-16 08:14:08 +0200

    Dodanie akcji na liście

--- resources/views/post/lista.blade.php
+++ resources/views/post/lista.blade.php
@@ -17,6 +17,10 @@
             <th scope="col" class="border border-gray-300 px-4 py-2">Tytuł</th>
             <th scope="col" class="border border-gray-300 px-4 py-2">Autor</th>
             <th scope="col" class="border border-gray-300 px-4 py-2">Data utworzenia</th>
+            @auth
+            <th scope="col" class="border border-gray-300 px-4 py-2">Akcja</th>   
+            @endauth
+
         </tr>
     </thead>
     @isset($posty)
@@ -28,17 +32,31 @@
                 <td class="border border-gray-300 px-4 py-2"><a href="{{route('post.show',$post->id)}}">{{$post->tytul}}</a></td>
                 <td class="border border-gray-300 px-4 py-2">{{$post->autor}}</td>
                 <td class="border border-gray-300 px-4 py-2">{{$post->created_at->setTimezone('Europe/Warsaw')->locale('pl')->translatedFormat('j F Y')}}</td>
+                @auth
+                <td class="border border-gray-300 px-4 py-2">
+                    <div class="flex items-center gap-x-2">
+                        <a href="{{route('post.edit',$post->id)}}">
+                            <button type="button" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline">E</button>
+                        </a>
+                        <form action="{{route('post.destroy',$post->id)}}" method="post" onsubmit="return confirm('Czy na pewno usunąć ten post o id {{$post->id}} i tytule {{$post->tytul}}?')">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline"  >X</button>
+                        </form>
+                    </div>
+                </td>
+                @endauth
             </tr>
             @empty
             <tr>
-                <th class="border border-gray-300 px-4 py-2 text-center" scope="row" colspan="4">Nie ma żadnych postów</th>
+                <th class="border border-gray-300 px-4 py-2 text-center" scope="row" colspan="@auth 5 @else 4 @endauth">Nie ma żadnych postów</th>
             </tr>
                 
             @endforelse
         </tbody>
         @else
         <tbody>
-            <th class="border border-gray-300 px-4 py-2 text-center" scope="row" colspan="4">Nie ma żadnych postów - brak definicji postów</th>
+            <th class="border border-gray-300 px-4 py-2 text-center" scope="row" colspan="@auth 5 @else 4 @endauth">Nie ma żadnych postów - brak definicji postów</th>
         </tbody>
     @endisset
 </table>
