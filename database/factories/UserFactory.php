f1b46da98ecab00e0276dae1dc6b76a8b4058682
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2026-05-30 12:27:42 +0200

    Faktoryzacja dla userów PL

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,22 +23,24 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+        //generowanie polskich imion i nazwisk dla tych samych płci dla imion i nazwisk
+        $plec = fake()->randomElement(['male','female']);
+        $imie = fake('pl_PL')->firstName($plec);
+        $nazwisko = fake('pl_PL')->lastName($plec);
+        $name = $imie.' '.$nazwisko;
+        // 1. Tworzymy bazowy e-mail: małe litery i usunięte polskie znaki (ł -> l, ę -> e)
+        $login = Str::lower(Str::ascii($imie)) . '.' . Str::lower(Str::ascii($nazwisko));
+        $domena = fake('pl_PL')->freeEmailDomain();
+        $pelnyEmail = $login . '@' . $domena;
+        // 2. Wymuszamy unikalność na CAŁYM adresie e-mail
+        // Jeśli taki mail już się istnieje, Faker doda unikalny numer przed znakiem @
+        $unikalnyEmail = fake()->unique()->numerify($pelnyEmail);
+    return [
+            'name' => $name,
+            'email' => $unikalnyEmail,
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('zaq1@WSX'),
             'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
